5a1a78ab73100fa236c6302de1a227ed74a34a4c
Patrick Beaume <p.beaume@gmail.com>
2024-04-22 13:35:57 +0200

    add rainbow deck locator

--- app/src/locators/Locators.tsx
+++ app/src/locators/Locators.tsx
@@ -3,7 +3,9 @@ import { LocationType } from '@gamepark/soltis/material/LocationType'
 import { MaterialType } from '@gamepark/soltis/material/MaterialType'
 import { PlayerId } from '@gamepark/soltis/PlayerId'
 import { landscapeDeckLocator } from './LandscapeDeckLocator'
+import { rainbowDeckLocator } from './RainbowDeckLocator'
 
 export const Locators: Partial<Record<LocationType, ItemLocator<PlayerId, MaterialType, LocationType>>> = {
-  [LocationType.LandscapeDeck]: landscapeDeckLocator
+  [LocationType.LandscapeDeck]: landscapeDeckLocator,
+  [LocationType.RainbowDeck]: rainbowDeckLocator
 }
---
+++ app/src/locators/RainbowDeckLocator.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { DeckLocator } from '@gamepark/react-game'
+
+
+export class RainbowDeckLocator extends DeckLocator {
+  coordinates = { x: -25, y: -20, z: 0}
+  delta = { x: -0.03, y: -0.03, z: 0.05 }
+}
+
+export const rainbowDeckLocator = new RainbowDeckLocator()
