fb31f8f9391ec29e8c8ace09f8746d5eb3696994
Patrick Beaume <p.beaume@gmail.com>
2024-05-06 13:27:15 +0200

    ignore bird into bird scoring effect

--- rules/src/rules/scoring/BirdScoring.ts
+++ rules/src/rules/scoring/BirdScoring.ts
@@ -1,10 +1,13 @@
 import { MaterialItem } from '@gamepark/rules-api'
+import { Spirit } from '../../material/Spirit'
 import { SpiritColor, SpiritDescriptions } from '../../material/SpiritDescription'
 import { AbstractScoringRule } from './AbstractScoringRule'
 
 export class BirdScoring extends AbstractScoringRule {
 
   getScore(spirits: MaterialItem[]) {
-    return spirits.filter((item) => SpiritDescriptions[item.id].color === SpiritColor.Blue).length
+    return spirits
+      .filter((item) => SpiritDescriptions[item.id].color === SpiritColor.Blue && item.id !== Spirit.Bird)
+      .length
   }
 }
