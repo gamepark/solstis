de27ae1dbb3d1f987c99b1452c7c924da24de048
Patrick Beaume <p.beaume@gmail.com>
2024-05-17 09:52:53 +0200

    add image in player panels

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -6,6 +6,8 @@ import { SolstisRules } from '@gamepark/solstis/SolstisRules'
 import { FC } from 'react'
 import { createPortal } from 'react-dom'
 import Victory from '../images/icons/victory.png'
+import Player1BG from '../images/panels/player-1.jpg'
+import Player2BG from '../images/panels/player-2.jpg'
 
 export const PlayerPanels: FC<any> = () => {
   const players = usePlayers({ sortFromMe: true })
@@ -24,6 +26,7 @@ export const PlayerPanels: FC<any> = () => {
           player={player}
           css={(player.id === (playerId ?? rules.players[0])) ? leftCss : rightCss}
           mainCounter={!rules.game?.rule?.id ? { imageCss: css`border: 0`, image: Victory, value: new ScoringHelper(rules.game, player.id).score } : undefined}
+          backgroundImage={player.id === 1? Player1BG: Player2BG}
           timerOnRight={!!rules.game?.rule?.id}/>
       )}
     </>,
