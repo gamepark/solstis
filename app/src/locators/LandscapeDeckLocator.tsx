f01516c9bc00b6ce943f663d29b5038360d81e0d
Patrick Beaume <p.beaume@gmail.com>
2024-06-10 20:51:39 +0200

    add card number in deck

--- app/src/locators/LandscapeDeckLocator.tsx
+++ app/src/locators/LandscapeDeckLocator.tsx
@@ -1,30 +1,17 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
-import { LocationDescription, PileLocator } from '@gamepark/react-game'
-import { LocationType } from '@gamepark/solstis/material/LocationType'
-import { landscapeTileDescription } from '../material/LandscapeTileDescription'
+import { PileLocator } from '@gamepark/react-game'
+import { LandscapeDeckDescription } from './description/LandscapeDeckDescription'
 
 
 export class LandscapeDeckLocator extends PileLocator {
   limit = 100
-  locationDescription = new LandscapeDescription()
+  locationDescription = new LandscapeDeckDescription()
 
   coordinates = { x: 4.85, y: -6.5, z: 0}
   delta = { x: -0.03, y: -0.03, z: 0.05 }
 
   radius = 5
   maxAngle = 90
-}
-
-class LandscapeDescription extends LocationDescription {
-  height = 11 + landscapeTileDescription.height
-  width = 11 + landscapeTileDescription.height
-  borderRadius = 5
-  extraCss = css`background-color: rgba(255, 255, 255, 0.5)`
-  coordinates = { x: 4.85, y: -6.5, z: 50}
-  //alwaysVisible = true
-  location = { type: LocationType.LandscapeDeck }
-
 }
 
 export const landscapeDeckLocator = new LandscapeDeckLocator()
---
+++ app/src/locators/description/LandscapeDeckDescription.ts
@@ -0,0 +1,38 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { LocationContext, LocationDescription } from '@gamepark/react-game'
+import { Location } from '@gamepark/rules-api'
+import { LocationType } from '@gamepark/solstis/material/LocationType'
+import { MaterialType } from '@gamepark/solstis/material/MaterialType'
+import { landscapeTileDescription } from '../../material/LandscapeTileDescription'
+
+export class LandscapeDeckDescription extends LocationDescription {
+  height = 11 + landscapeTileDescription.height
+  width = 11 + landscapeTileDescription.height
+  borderRadius = 50
+  getExtraCss(_location: Location, context: LocationContext) {
+    const { rules } = context
+    const deckLength = rules.material(MaterialType.LandscapeTile).location(LocationType.LandscapeDeck).length
+    return css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      pointer-events: none;
+      &:after {
+        content: '${deckLength}';
+        display: flex;
+        align-self: center;
+        justify-content: center;
+        color: black;
+        font-size: 5em;
+        font-weight: bold;
+        opacity: 0.6;
+      }
+      
+    `
+  }
+  extraCss = css``
+  coordinates = { x: 4.85, y: -6.5, z: 50}
+  alwaysVisible = true
+  location = { type: LocationType.LandscapeDeck }
+}
