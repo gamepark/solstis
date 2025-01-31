3d0de1fbb7a7a2d152d16d032367c717d521237e
Patrick Beaume <p.beaume@gmail.com>
2025-01-31 10:26:36 +0100

    fix translations

--- app/src/material/help/FireflyHelp.tsx
+++ app/src/material/help/FireflyHelp.tsx
@@ -1,6 +1,9 @@
-import { MaterialHelpProps } from '@gamepark/react-game'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { MaterialHelpProps, shadowCss } from '@gamepark/react-game'
 import { FC } from 'react'
 import { Trans } from 'react-i18next'
+import Victory from '../../images/icons/victory.png'
 
 export const FireflyHelp: FC<MaterialHelpProps> = () => {
   return (
@@ -8,9 +11,29 @@ export const FireflyHelp: FC<MaterialHelpProps> = () => {
       <h2>
         <Trans defaults="help.firefly" />
       </h2>
-      <p>
-        <Trans defaults="help.firefly.desc" />
+      <p css={textWithIconCss}>
+        <Trans defaults="help.firefly.desc" components={{
+          star: <span css={iconCss(Victory)}/>
+        }} />
       </p>
     </>
   )
 }
+
+const iconCss = (icon: string) => css`
+  display: inline-block;
+  background: url(${icon}) no-repeat;
+  background-size: cover;
+  border-radius: 5em;
+  height: 1.4em;
+  width: 1.4em;
+  margin-left: 0.3em;
+  ${shadowCss(icon)}
+`
+
+const textWithIconCss = css`
+  > span {
+    margin-left: 0;
+    margin-bottom: -0.35em;
+  }
+`
